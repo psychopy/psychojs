32f4b83926ed3173a338df1e67a450cef279d55e
Sotiri Bakagiannis <sotirios.bakagiannis@gmail.com>
2021-01-21 19:47:01 +0000

    data/TrialHandler: clean up

--- js/data/TrialHandler.js
+++ js/data/TrialHandler.js
@@ -470,7 +470,7 @@ export class TrialHandler extends PsychObject
 						// Look for string encoded arrays in the form of '[1, 2]'
 						const arrayMaybe = util.turnSquareBracketsIntoArrays(value);
 
-						if (Array.isArray(arrayMaybe))
+						if (Array.isArray(arrayMaybe) && arrayMaybe.length)
 						{
 							// Keep the first match if more than one are found. If the
 							// input string looked like '[1, 2][3, 4]' for example,
