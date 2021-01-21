9b5938098fdf6b740fbdcc0275209289bd75a5c9
Sotiri Bakagiannis <sotirios.bakagiannis@gmail.com>
2021-01-21 20:55:58 +0000

    data/TrialHandler: clean up

--- js/data/TrialHandler.js
+++ js/data/TrialHandler.js
@@ -470,14 +470,13 @@ export class TrialHandler extends PsychObject
 						// Look for string encoded arrays in the form of '[1, 2]'
 						const arrayMaybe = util.turnSquareBracketsIntoArrays(value);
 
-						if (Array.isArray(arrayMaybe) && arrayMaybe.length)
+						if (Array.isArray(arrayMaybe))
 						{
 							// Keep the first match if more than one are found. If the
 							// input string looked like '[1, 2][3, 4]' for example,
 							// the resulting `value` would be [1, 2]. When `arrayMaybe` is
-							// empty, `value` turns `undefined`. At this point that might
-							// only happen if `value` is an empty array to begin with.
-							value = arrayMaybe[0];
+							// empty, `value` turns `undefined`.
+							value = arrayMaybe;
 						}
 
 						// if value is a numerical string, convert it to a number:
