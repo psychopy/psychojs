e1df2bc19cf14b5d3f6ca86dd6d7f9f0a59fd145
Sotiri Bakagiannis <sotirios.bakagiannis@gmail.com>
2021-04-15 13:45:13 +0100

    js/src: add index files to re-export components from each module folder

---
+++ src/index.js
@@ -0,0 +1,5 @@
+export * as util from './util/index.js';
+export * as core from './core/index.js';
+export * as data from './data/index.js';
+export * as visual from './visual/index.js';
+export * as sound from './sound/index.js';
---
+++ src/sound/index.js
@@ -0,0 +1,4 @@
+export * from './Sound.js';
+export * from './SoundPlayer.js';
+export * from './TonePlayer.js';
+export * from './TrackPlayer.js';
