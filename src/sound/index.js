bf4fe0b3dba2a62805203a64e9d968a778190b78
Sotiri Bakagiannis <sotirios.bakagiannis@gmail.com>
2021-06-01 17:44:55 +0100

    sound: drop Transcriber export for now

--- src/sound/index.js
+++ src/sound/index.js
@@ -4,5 +4,5 @@ export * from './TonePlayer.js';
 export * from './TrackPlayer.js';
 export * from './Microphone.js';
 export * from './AudioClip.js';
-export * from './Transcriber.js';
+// export * from './Transcriber.js';
 
