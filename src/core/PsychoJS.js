f11e3566532b4f058ed0a23c4801e64892cc80ed
Thomas Pronk <thomas.pronk@nottingham.ac.uk>
2021-06-03 18:13:36 +0100

    RF: clean up improved error logging

--- src/core/PsychoJS.js
+++ src/core/PsychoJS.js
@@ -743,13 +743,12 @@ export class PsychoJS
 		};
 		window.onunhandledrejection = function (error)
 		{
-			//window.myError = error;
-			//console.log(JSON.stringify(error.reason));
 			console.error(error.reason);
-			// No stack? Error thrown by PsychoJS; stringify whole error
 			if (error.reason.stack === undefined) {
+				// No stack? Error thrown by PsychoJS; stringify whole error
 				document.body.setAttribute('data-error', JSON.stringify(error.reason));
 			} else {
+				// Yes stack? Error thrown by JS; stringify stack
 				document.body.setAttribute('data-error', JSON.stringify(error.reason.stack));
 			}
 			self._gui.dialog({error: error.reason});
