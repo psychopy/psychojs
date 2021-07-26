793e751d3211cf35a6a8965472ee9b284a51cfa8
Alain Pitiot <alain.pitiot@ilixa.com>
2021-07-26 14:36:59 +0200

    clean up

--- src/data/index.js
+++ src/data/index.js
@@ -1,5 +1,2 @@
 export * from "./ExperimentHandler.js";
 export * from "./TrialHandler.js";
-export * from "./QuestHandler.js";
-export * from "./MultiStairHandler.js";
-//export * from "./Shelf.js";
