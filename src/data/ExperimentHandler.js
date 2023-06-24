c657ad6ee2e842856a98bd30ff1a66ef84f5e0d0
Eitan Hemed <37670372+EitanHemed@users.noreply.github.com>
2023-06-24 09:00:22 +0000

    Assigning field_separator as parameter of ExperimentHandler

--- src/data/ExperimentHandler.js
+++ src/data/ExperimentHandler.js
@@ -56,12 +56,14 @@ export class ExperimentHandler extends PsychObject
 	 * @param {module:core.PsychoJS} options.psychoJS - the PsychoJS instance
 	 * @param {string} options.name - name of the experiment
 	 * @param {Object} options.extraInfo - additional information, such as session name, participant name, etc.
+	 * @param {string} field_separator - Delimiter character specification
 	 */
 	constructor({
 		psychoJS,
 		name,
 		extraInfo,
-		dataFileName
+		dataFileName,
+		field_separator
 	} = {})
 	{
 		super(psychoJS, name);
@@ -98,6 +100,12 @@ export class ExperimentHandler extends PsychObject
 		this._currentTrialData = {};
 
 		this._experimentEnded = false;
+
+		// assign output delimiter character
+		this.field_separator = (typeof field_separator === "string" || field_separator.length === 1 || field_separator !== '\n')
+			? field_separator
+  		: ',';
+
 	}
 
 	/**
@@ -291,7 +299,7 @@ export class ExperimentHandler extends PsychObject
 			// TODO only save the given attributes
 			const worksheet = XLSX.utils.json_to_sheet(data);
 			// prepend BOM
-			const csv = "\ufeff" + XLSX.utils.sheet_to_csv(worksheet);
+			const csv = "\ufeff" + XLSX.utils.sheet_to_csv(worksheet, {FS: this.field_separator});
 
 			// upload data to the pavlovia server or offer them for download:
 			const filenameWithoutPath = this._dataFileName.split(/[\\/]/).pop();
