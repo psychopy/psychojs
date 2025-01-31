cdf47e6aacd3f55042d120ab04dc2fa816df83df
Nati Tsegaye <nht249@nyu.edu>
2025-02-01 03:35:21 +0400

    format error message

--- src/core/GUI.js
+++ src/core/GUI.js
@@ -388,7 +388,7 @@ export class GUI
 					}
 					try{
 						const BC = status.block_condition
-						const text = `block: ${status.block}, condition: ${status.block_condition.split("_")[1]}, trial: ${status.trial}<br>
+						const text = `<br>block: ${status.block}, condition: ${status.block_condition.split("_")[1]}, trial: ${status.trial}<br>
 									  conditionName: ${paramReader.read("conditionName", BC)}<br>
 									  experiment: ${thisExperimentInfo.experiment}<br>`
 						error += text
